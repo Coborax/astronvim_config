00aefb321a7f6c56c221531a2ea0f657237d949e
Mikkel Mouridsen <mikkelmouridsen@me.com>
2023-04-09 13:04:31 +0200

    Added angularls to mason config

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = { "lua_ls", "clangd", "eslint" },
+      ensure_installed = { "lua_ls", "clangd", "eslint", "angularls" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
